3d8bd91af04522b641099811b5547af99959eb1a
Vulkan <523066680@163.com>
2019-04-18 14:58:31 +0800

    Signed-off-by: Vulkan <523066680@163.com>

---
+++ Matrix/CodeRain.bat
@@ -0,0 +1,20 @@
+@echo off
+
+set NAME=CodeRain
+set SRC=CodeRain.c outline.c
+set INCDIR=-ID:\Lib\freeglut-MinGW-3.0.0-1.mp\include -ID:/lib/freetype-2.7/include -I./
+set LIBDIR=-LD:\Lib\freeglut-MinGW-3.0.0-1.mp\lib\x64 -LD:/lib/freetype-2.7/objs/.libs
+set LIB=-lfreeglut -lopengl32 -lglu32 -lfreetype -lpng -lz
+
+:Compile
+gcc -std=c11 %SRC% -o "%NAME%" %INCDIR% %LIBDIR% %LIB%
+
+:Run
+if %errorlevel% == 0 (
+	CodeRain.exe
+) else (
+	echo Compile Error
+	rem pause
+)
+
+::2016-11-07 add -lpng -lz
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # GL-Screensaver
 GL Animation Programming
+
+* 资源
+  * 屏保
+  [screensaversplanet](https://www.screensaversplanet.com/screensavers/)
+  [XScreenSaver](https://www.jwz.org/xscreensaver/download.html)
+  [XScreenSaverWin](https://github.com/katahiromz/XScreenSaverWin)
+
